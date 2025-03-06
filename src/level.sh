ae948afc92c130547abdbc4fa6039e7bcc27e394
Ruslan <rusla.karelov@gmail.com>
2025-03-06 15:20:38 +0100

    rename some func, add a few comments

--- src/level.sh
+++ src/level.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if command -v pmset &> /dev/null; then
+    # OSX
     pmset -g batt | grep -Eo "\d+%" | tr -d "%"
 elif command -v acpi &>/dev/null; then
+    # Linux
     acpi -b | grep -m 1 -Eo "[0-9]+%" | tr -d "%"
 fi
